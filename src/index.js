74f6448ba91835c637bf7758c5351aad6805a07f
esantiagooo <77802371+esantiagooo@users.noreply.github.com>
2021-06-14 00:05:55 -0400

    Completed mini-project ericyanez

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // I am declaring a variable that is equal to an element id named create-task-form
+  const eventListener = document.getElementById('create-task-form');
+  //
+  eventListener.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const listenEvent = document.getElementById("new-task-description").value;
+    
+    const list = document.getElementById('tasks');
+    const li = document.createElement('li');
+    li.innerHTML = listenEvent;
+
+    list.appendChild(li)
+
+    document.getElementById("new-task-description").value = ""
+  
+  })
+  
 });
